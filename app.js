41c6a4dc06d0ce168f102d5cdda283add8ef1825
imlsr <likhith.sr@studentpartner.com>
2020-04-15 18:13:22 +0530

    Basic frame working

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const bodyparser = require("body-parser");
 
 const app = express();
 
-app.use("view engine", "ejs");
+var items = [];
+
+app.set("view engine", "ejs");
+app.use(bodyparser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  var today = new Date();
+
+  var options = {
+    weekday : "long",
+    day:"numeric",
+    month:"long"
+
+  };
+
+  var day = today.toLocaleDateString("en-US",options);
+  res.render("list",{kindofday:day, newtask:items});
+
 });
+
+
+app.post("/", function(req,res){
+
+  var task = req.body.newitem;
+
+  items.push(task);
+
+  res.redirect("/");
+});
+
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
